5c106461c92444544466f6de6324079593b59569
Gavruel <gabrielh.gh963@gmail.com>
2026-01-13 00:10:19 -0300

    feat: add database config and category entity

---
+++ src/main/java/com/gsantos/personalfinanceapi/model/entities/Category.java
@@ -0,0 +1,34 @@
+package com.gsantos.personalfinanceapi.model.entities;
+
+import com.gsantos.personalfinanceapi.model.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/gsantos/personalfinanceapi/model/enums/TransactionType.java
@@ -0,0 +1,6 @@
+package com.gsantos.personalfinanceapi.model.enums;
+
+public enum TransactionType {
+    INCOME,
+    EXPENSE
+}
